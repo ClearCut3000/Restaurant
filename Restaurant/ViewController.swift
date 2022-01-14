a00c672b1e7c50e12c18013a7c30d036e08efdd2
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-01-14 22:37:51 +0300

    Initial Commit

---
+++ Restaurant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Restaurant
+//
+//  Created by Николай Никитин on 14.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
